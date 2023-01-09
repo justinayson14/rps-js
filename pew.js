8405e33f377deac2c0ef3ef147a961ec0cd7da4e
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2023-01-08 22:24:45 -0800

    Add playRound func. declaration & pseudocode

--- pew.js
+++ pew.js
@@ -2,24 +2,33 @@
 function getComputerChoice() {
     const random = Math.floor(Math.random() * 3);
     
-    let computerChoice = null;
+    let computerChoice = "";
     switch (random) {
         case 0:
-            computerChoice = "rock hard!";
+            computerChoice = "rock";
             break;
         case 1:
-            computerChoice = "wet water!";
+            computerChoice = "scissor";
             break;
         case 2:
-            computerChoice = "paper thin";
+            computerChoice = "paper";
             break;
     }
 
     return computerChoice;
 }
 
-console.log(getComputerChoice());
-//Get User choice
 //User choice vs. Computer choice
+function playRound(userPick, computerPick) {
+    //if (conditions when user wins)
+    //else if (conditions pc wins)
+    //else (conditions tie)
+    //return round winner
+}
+
+const userPick = "rock";
+userPick = userPick.toLowerCase();
+const computerPick = getComputerChoice();
+playRound(userPick, computerPick);
 //Loop for 5 rounds
 //State winner
