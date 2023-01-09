4c68d7552fc1d86b7f1b4c72a699c5db673c3626
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2023-01-08 20:33:18 -0800

    Add function getComputerChoice

--- pew.js
+++ pew.js
@@ -1,4 +1,24 @@
 //Get Computer choice
+function getComputerChoice() {
+    const random = Math.floor(Math.random() * 3);
+    
+    let computerChoice = null;
+    switch (random) {
+        case 0:
+            computerChoice = "rock hard!";
+            break;
+        case 1:
+            computerChoice = "wet water!";
+            break;
+        case 2:
+            computerChoice = "paper thin";
+            break;
+    }
+
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
 //Get User choice
 //User choice vs. Computer choice
 //Loop for 5 rounds
